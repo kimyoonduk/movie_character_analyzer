f871b28552f448d5966c0188c072ac25b0e12de9
HuangJon <huang.jonathanw@gmail.com>
2017-04-28 00:15:07 -0400

    running recognizer.test is weird
    - printing result shows false, but it enters the loop, which is the correct thing to do
    - output is also correct

--- python/recognizer.py
+++ python/recognizer.py
@@ -35,9 +35,11 @@ class Recognizer:
                 for actor, encoding in self.known_face_encodings.items():
                     results = face_recognition.compare_faces(encoding, face) 
                     print(actor)
-                    print(results)
+                    for r in results:
+                        print(r)
                     print("results len: " + str(len(results)))
-                    if results[0]:
+                    if results:
+                        ## printing results says false but still enters here
                         print("result was true! found: " + actor)
                         if frame not in actor_map:
                             actors = [actor]
@@ -51,6 +53,35 @@ class Recognizer:
         self.khaos.create_actor_map(actor_map)
         return self.khaos
 
+    def test1(self, images):
+        actor_map = { } # maps frame : list of actors 
+        for frame, img in sorted(sorted(images.items()), key=len):
+            print(frame)
+            # frame_face_locations = face_recognition.face_locations(img)
+            # unknown_faces = face_recognition.face_encodings(img, frame_face_locations)
+            unknown_faces = face_recognition.face_encodings(img)
+            print("unknown_faces len: " + str(len(unknown_faces)))
+            if int(frame) > 1500:
+                break
+            for face in unknown_faces:
+                for actor in self.known_face_encodings.keys():
+                    encoding = self.known_face_encodings.get(actor)
+                    results = face_recognition.compare_faces(encoding, face) 
+                    print(results)
+                    print("results len: " + str(len(results)))
+                    if results:
+                        print("result was true! found: " + actor)
+                        if frame not in actor_map:
+                            actors = [actor]
+                            actor_map[frame] = actors
+                        else:
+                            print("appending to actor list")
+                            actor_map[frame].append(actor)
+                            print(actor_map[frame])
+                            # actor_map.get(frame) = actor
+                        
+        self.khaos.create_actor_map(actor_map)
+        return self.khaos
 
     def find_and_recognize(self, images):
         actor_map = { }    # frame : list of actors
