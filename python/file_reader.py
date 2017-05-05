d2743ec733f2795658e02b184636c2b6d34e128e
HuangJon <huang.jonathanw@gmail.com>
2017-05-04 22:37:55 -0400

    Comments

--- python/file_reader.py
+++ python/file_reader.py
@@ -4,7 +4,11 @@ import cv2
 
 class File_Reader:
 
-    # extract video to images stored in a dir
+    """
+    extract video to images stored in a dir 
+    video: video file path
+    path_output_dir: dir to save extracted images
+    """
     def video_to_frames(self, video, path_output_dir):
         # extract frames from a video and save to directory as 'x.png' where 
         # x is the frame index
@@ -25,9 +29,12 @@ class File_Reader:
         return path_output_dir 
 
     
-    # takes a directory of images and creates:
-    # mode 0(training): a dictionary mapping actor : numpy array representations of each image 
-    # mode 1(movie images): a dictionary mapping frame number : numpy array representation of each image
+    """
+    - takes a directory of images and creates:
+        - mode 0(training): a dictionary mapping actor : numpy array representations of each image 
+        - mode 1(movie images): a dictionary mapping frame number : numpy array representation of each image 
+    - dir_path: the dir to read the images from
+    """
     def image_to_array(self, dir_path, mode):
         image_map = { }
         for img in sorted(sorted(os.listdir(dir_path)), key=len):
